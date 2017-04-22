a4734b363e8794bdbc3ceb24375ead02d5528562
Dharmesh Vasani <dharmesh.vasani@multidots.in>
2017-04-22 18:21:17 +0530

    changes in the header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    var currentUserToken = localStorage.getItem('currentUserToken');
+    if (!currentUserToken) {
+      this.router.navigate(['']);
+    }
+  }
 
   ngOnInit() {
   }
